ceaaef8b92ed219f4d4746f6eab551ab153f149f
BeyondTheColours <86986714+BeyondTheColours@users.noreply.github.com>
2024-09-24 16:07:45 +0100

    Update Ologic.ml
    Updated <type test> to include Gt (greater tham) and Lt (less than).
    
    Updated comp_expr
    Old type = comp_expr : expr -> expr -> bool
    New type = comp_expr : ('a -> 'a -> bool) -> expr -> expr -> bool.
    Want to include general binary operators ( =, <, >, >= etc) as arguments to generalise the function.
    
    NOT TESTED

--- Ologic.ml
+++ Ologic.ml
@@ -1,11 +1,14 @@
 type expr =
   |Str_lit of string
   |Int_lit of int
+  |Bool_lit of bool
 ;;
 
 type test =
   |Eq of expr*expr
   |Neq of expr*expr
+  |Gt of expr*expr
+  |Lt of expr*expr
 ;;
 
 type log_op = And | Or
@@ -19,18 +22,31 @@ type cond =
   |Conds of cond*log_op*cond
 ;;
 
-let comp_expr e1 e2 =
+(* TODO - cannot meaningfully compare literals of different types.
+Default behaviour defined here as giving false.
+Will make this function return a bool option and give None when different types are compared.
+Can use this to through an exception in the main evaluation function later on.
+
+Will need to consider functions which depend on comp_expr later
+  -> eval_test directly uses comp_expr
+  -> eval_op_test depends on eval_test
+  -> eval_cond depends on eval_test
+*)
+
+let comp_expr bin_op e1 e2 =
   match (e1, e2) with
-  |(Str_lit(s1), Str_lit(s2)) -> s1 = s2
-  |(Int_lit(n1), Int_lit(n2)) -> n1 = n2
+  |(Str_lit(s1), Str_lit(s2)) -> bin_op s1 s2
+  |(Int_lit(n1), Int_lit(n2)) -> bin_op n1 n2
   |(Str_lit(s1), Int_lit(n1)) -> false
   |(Int_lit(n1), Str_lit(s2)) -> false
 ;;
 
 let eval_test t =
   match t with
-  |Eq(expr1, expr2) -> comp_expr expr1 expr2
-  |Neq(expr1, expr2) -> not(comp_expr expr1 expr2)
+  |Eq(expr1, expr2) -> comp_expr ( = ) expr1 expr2
+  |Neq(expr1, expr2) -> not(comp_expr ( = ) expr1 expr2)
+  |Gt(expr1, expr2) -> comp_expr ( > ) expr1 expr2
+  |Lt(expr1, expr2) -> comp_expr ( < ) expr1 expr2
 ;;
 
 let eval_op_test o t1 t2 =
