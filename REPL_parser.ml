6d8ac482fa629345419e12eccdb7afd6a1bca325
BeyondTheColours <86986714+BeyondTheColours@users.noreply.github.com>
2024-09-26 21:38:50 +0100

    Update REPL_parser.ml

--- REPL_parser.ml
+++ REPL_parser.ml
@@ -1,5 +1,4 @@
 open ConsString
-open Ologic
 
 type keyword = Let | If |Else |While |AND |OR |Quit;;
 
@@ -17,16 +16,34 @@ let rec remove_trailing_whitespace c_s =
   ConsString.rev(remove_leading_whitespace ConsString.rev(c_s))
 ;;
 
+let bind o f =
+  match o with
+  |None -> None
+  |Some(v) -> f v
+;;
+
+let ( >>= ) o f = bind o f
+;;
+
 let get_input =
   let input = ConsString.string_to_cons_string(readline("~~>")) in
-  let cleanedinput = input |> remove_leading_whitespace |> remove_trailing_whitespace in
+  let cleanedinput = input >>= remove_leading_whitespace >>= remove_trailing_whitespace in
   cleaned_input
+;;
+
+let extract_input input =
+  match input with
+  |None -> Nil
+  |Some(v) -> v
 ;;
 
 let rec loop =
   let input = get_input in
-  let string_of_input = ConsString.cons_string_to_string line in
-  let print_input = print_endline string_of_int in
-  loop
+  let extracted_input = extract_input input in
+  if extracted_input = Nil then
+    let print_error = print_endline "Invalid input" in loop
+  else
+    let string_of_input = ConsString.cons_string_to_string extracted input in
+    let print_input = print_endline string_of_int in
+    loop
 ;;
-  
