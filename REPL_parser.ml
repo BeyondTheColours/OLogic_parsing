e36fb82e65a7c6c7838379087c520a5f70d7964e
BeyondTheColours <86986714+BeyondTheColours@users.noreply.github.com>
2024-09-26 22:29:21 +0100

    Add files via upload
    Loop to read input line and remove both leading and trailing whitespace.

--- REPL_parser.ml
+++ REPL_parser.ml
@@ -1,5 +1,7 @@
 open ConsString
 
+let test = print_endline("Here");;
+
 type keyword = Let | If |Else |While |AND |OR |Quit;;
 
 let rec remove_leading_whitespace c_s =
@@ -7,13 +9,15 @@ let rec remove_leading_whitespace c_s =
   |Nil -> None
   |Cons(c, t) ->
     if c != ' ' then Some(Cons(c, t)) else
-    match remove_leading_whitespace_worker t with
+    match remove_leading_whitespace t with
     |None -> None
     |Some(v) -> Some(v)
 ;;
 
 let rec remove_trailing_whitespace c_s =
-  ConsString.rev(remove_leading_whitespace ConsString.rev(c_s))
+  match remove_leading_whitespace (ConsString.rev(c_s)) with
+  |None -> None
+  |Some(v) -> Some(ConsString.rev v)
 ;;
 
 let bind o f =
@@ -25,25 +29,19 @@ let bind o f =
 let ( >>= ) o f = bind o f
 ;;
 
-let get_input =
-  let input = ConsString.string_to_cons_string(readline("~~>")) in
-  let cleanedinput = input >>= remove_leading_whitespace >>= remove_trailing_whitespace in
-  cleaned_input
-;;
-
 let extract_input input =
   match input with
   |None -> Nil
   |Some(v) -> v
 ;;
 
-let rec loop =
-  let input = get_input in
-  let extracted_input = extract_input input in
-  if extracted_input = Nil then
-    let print_error = print_endline "Invalid input" in loop
-  else
-    let string_of_input = ConsString.cons_string_to_string extracted input in
-    let print_input = print_endline string_of_int in
-    loop
-;;
+while true do
+  print_string("~~>");
+  let get_input =
+    let input = ConsString.string_to_cons_string(read_line()) in
+    input |> remove_leading_whitespace >>= remove_trailing_whitespace in
+  let extracted_input = extract_input get_input in
+  if extracted_input = Nil then print_endline("Invalid input\n")
+  else let output = ConsString.cons_string_to_string extracted_input in
+  print_endline(output^"\n")
+done
